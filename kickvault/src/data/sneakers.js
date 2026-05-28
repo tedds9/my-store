f354298a7441580855db9c431b9e38fc5d5afbba
Tedy Baisac <baisactedy2@gmail.com>
2026-05-28 13:14:10 +0800

    added data of shoes added new file and organize images

---
+++ kickvault/src/data/sneakers.js
@@ -0,0 +1,49 @@
+export const products = [{
+id : crypto.randomUUID(),
+name : 'KickVault Kinetic',
+brand: "KickVault",
+price: 450.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+},{
+id : crypto.randomUUID(),
+name : 'KickVault Vista',
+brand: "KickVault",
+price: 400.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+},{
+id : crypto.randomUUID(),
+name : 'KickVault Traverse',
+brand: "KickVault",
+price: 550.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+},{
+id : crypto.randomUUID(),
+name : 'KickVault Pace',
+brand: "KickVault",
+price: 500.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+},{
+id : crypto.randomUUID(),
+name : 'KickVault Converge',
+brand: "KickVault",
+price: 480.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+},{
+id : crypto.randomUUID(),
+name : 'KickVault Cadence',
+brand: "KickVault",
+price: 480.00,
+image: "",
+category: "Luxury Sneakers",
+isFavorite: false
+}]
