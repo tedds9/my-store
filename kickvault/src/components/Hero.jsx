8322db03824bfb25fd153a3b5629e93ffa59268b
Tedy Baisac <baisactedy2@gmail.com>
2026-05-28 09:06:39 +0800

    added more css to the hero section

--- kickvault/src/components/Hero.jsx
+++ kickvault/src/components/Hero.jsx
@@ -5,11 +5,11 @@ export function Hero() {
   return (
 
     
-    <section className="hero" >
-      <div>
+    <section className="hero" aria-label="hero-section" >
+      <div className="hero-container">
         <h1 className="heading" aria-label="heading">EVERY STEP. ELEVATED.</h1>
         <p className="sub-heading" aria-label="subheading">"Premium sneakers for those who move with intention."</p>
-        <div>
+        <div className="action-now-container">
           <button className="shop-now" aria-label="Shop now">
             Shop Now
           </button>
