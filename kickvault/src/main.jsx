97f6664e65e3912423d1d38eb856b35ee6b8ae76
Tedy Baisac <baisactedy2@gmail.com>
2026-06-04 23:27:53 +0800

    styles(core): initialize system layer and logical reset foundations

--- kickvault/src/main.jsx
+++ kickvault/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/index.css';
 import App from './App.jsx'
 
-import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
