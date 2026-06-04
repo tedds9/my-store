757f1af595be667db661c635e72bbf05a314c2a3
Tedy Baisac <baisactedy2@gmail.com>
2026-06-04 22:30:06 +0800

    chore(deps): align core dependencies and postcss tooling configuration

--- kickvault/postcss.config.js
+++ kickvault/postcss.config.js
@@ -2,6 +2,5 @@ export default {
   plugins: {
     'postcss-import': {},
     // Automatically wraps files discovered inside the components directory into the components layer
-    '@tailwindcss/postcss': {}, 
   },
 };
