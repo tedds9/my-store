d921961f54a04fd499fd8b7b0d5c24ee17011c8e
Tedy Baisac <baisactedy2@gmail.com>
2026-06-05 23:16:28 +0800

    feat(shop): update hero section layout and styling
    -Update Hero layout structure inside the view file
    -Adjust hero component sizing and spatial rules inside style module

--- kickvault/src/components/shop/Hero.jsx
+++ kickvault/src/components/shop/Hero.jsx
@@ -1,19 +1,29 @@
 
 import styles from './hero.module.css';
+import heroImage from '../../images/shoes-image/heroPicture.jpg';
 
 export function Hero() {
   return (
 
-    
+
     <section className={styles.hero} aria-label="feature-product-introduction" >
+      <div className={styles.imageWrapper}>
+        <img src={heroImage}
+          alt=""
+          className={styles.heroImage}
+          loading="eager"
+          fetchPriority="high" />
+      </div>
       <div className={styles.container}>
-        <h1 className={styles.heading} >EVERY STEP. ELEVATED.</h1>
+        <h1 className={styles.heading} >EVERY STEP ELEVATED</h1>
         <p className={styles.subHeading} >"Premium sneakers for those who move with intention."</p>
         <div className={styles.actionNowContainer}>
-          <button className={styles.shopNow} >
+          <button
+            className={styles.shopNow} >
             Shop Now
           </button>
-          <button className={styles.exploreNow} >
+          <button
+            className={styles.exploreNow} >
             Explore
           </button>
         </div>
