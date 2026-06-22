4e4f1ff2369dab3c93e1847c4a4081a77e7746fa
Tedy Baisac <baisactedy2@gmail.com>
2026-06-22 18:01:45 +0800

    refactor: expand basket schema to support multi-parameter tracking and hydrated views

--- kickvault/src/types/basket-selections.ts
+++ kickvault/src/types/basket-selections.ts
@@ -1,4 +1,13 @@
+import { MerchandiseAsset } from "./merchandise-assets";
+
 export interface BasketSelection {
   readonly id: string;
+  readonly assetId: string;
+  readonly selectedSize: string;
+  readonly quantity: number;
+}
+
+export interface CartLineItem extends Omit<MerchandiseAsset, 'sizes'> {
+  readonly selectedSize: string;
   readonly quantity: number;
 }
