187cb548640fc724c2a5e87b89301819b0c195b3
Tedy Baisac <baisactedy2@gmail.com>
2026-07-02 15:09:28 +0800

    refactor(cart): align drawer class selectors to strict mathematical naming formula

--- kickvault/src/components/router/RouteSnap.tsx
+++ kickvault/src/components/router/RouteSnap.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useShop } from '@/context/ShopContext';
 
 export function RouteSnap() {
   const { pathname } = useLocation();
+  const { toggleBasket } = useShop();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-
+    toggleBasket('idle')
   }, [pathname]);
 
   return null;
