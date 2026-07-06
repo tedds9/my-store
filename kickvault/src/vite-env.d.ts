1e3b8579bf4e6e8013006924d765f0d0c60e6673
Tedy Baisac <baisactedy2@gmail.com>
2026-07-06 20:47:51 +0800

    types(env): integrate global client definitions for vite variables

--- kickvault/src/vite-env.d.ts
+++ kickvault/src/vite-env.d.ts
@@ -1,11 +1,1 @@
-declare module '*.css';
-
-declare module '*.module.css' {
-  const classes: { [key: string]: string};
-  export default classes;
-}
-
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
+/// <reference types="vite/client" />
