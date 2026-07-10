12401742e3d2dc8fb4e915abfb8e30e1535cf96a
Tedy Baisac <baisactedy2@gmail.com>
2026-07-10 16:36:47 +0800

    refactor: flatten repository directory layout to root deck and rename project to HEAVYWEIGHT

---
+++ src/data/inventory-assets.js
@@ -0,0 +1,38 @@
+export const STORE_ASSETS = [
+  {
+    id: 'hw-pullover',
+    name: 'Boxy Pullover Hoodie',
+    brand: 'HEAVYWEIGHT',
+    price: 85.00,
+    image: 'https://unsplash.com',
+    category: 'hoodies',
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'hw-zipup',
+    name: 'Oversized Full-Zip Hoodie',
+    brand: 'HEAVYWEIGHT',
+    price: 90.00,
+    image: 'https://unsplash.com',
+    category: 'hoodies',
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'hw-trackpants',
+    name: 'Heavyweight Trackpants',
+    brand: 'HEAVYWEIGHT',
+    price: 75.00,
+    image: 'https://unsplash.com',
+    category: 'bottoms',
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'hw-tee',
+    name: 'Oversized Graphic Tee',
+    brand: 'HEAVYWEIGHT',
+    price: 40.00,
+    image: 'https://unsplash.com',
+    category: 'tees',
+    sizes: ['S', 'M', 'L', 'XL']
+  }
+];
